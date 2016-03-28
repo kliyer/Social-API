f43f53627dd2c56a0ab1360d388c2c373cb58e9f
Kartik Iyer <iyerkartik04@gmail.com>
2016-03-28 02:25:26 -0700

    Added files via upload

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ var channelName = 'TechGuyWeb';
 
 $(document).ready(function (){
 	$.get(
-	"https://www.googleapis.com/youtube/v3/channels",{
-	part:'contentDetails',
-	forUsername: channelName,
+	"https://www.googleapis.com/youtube/v3/videos",{
+	part:'snippet',
+	chart:'mostPopular',
+	regionCode:'US',
 	key:'AIzaSyAa1rfPGIPa7_LYPXkvPeE_5AIxwUgBHhE'},
 	function(data){
 		$.each(data.items,function(i,item){
 		console.log(item);
-		var pid = item.contentDetails.relatedPlaylists.uploads;
-		getVids(pid);
+		var pid = item.snippet.resourceId.videoId;
+		output = '<li><iframe src=\"//www.youtube.com/embed/'+pid+'\"</li>';
+		$('#results').append(output);
 		})
 
 	}
