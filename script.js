b94a1e647849ebcd3f41697b66f2a26ae2aedbdf
Kartik Iyer <iyerkartik04@gmail.com>
2016-03-28 22:09:54 -0700

    Added files via upload

--- script.js
+++ script.js
@@ -10,10 +10,32 @@ $(document).ready(function (){
 		$.each(data.items,function(i,item){
 		console.log(item);
 		var pid = item.id;
-		output = '<li><iframe src=\"//www.youtube.com/embed/'+pid+'\"</li>';
+		output = '<li class="erase"><a class="tweetbutton"href="\"https://twitter.com/intent/tweet\"">Tweet</a><iframe src=\"//www.youtube.com/embed/'+pid+'\"</li>';
 		$('#results').append(output);
 		})
 	}
 	);
 
 });
+$("form").on("submit",function(){
+		  $(".erase").remove();
+          $.get(
+                  "https://www.googleapis.com/youtube/v3/videos",{
+                            part: 'snippet',
+                            maxResults: 10,
+                      		q:encodeURIComponent($("#search-text-input").val()).replace(/%20/g,"+"),
+                            key: 'AIzaSyCc1xGbk7VT3f-HVv4aL4HsHIHRCNrcpmw'},
+                            function(data){
+                                        var videos;
+                                       $.each(data.items, function(i, item){
+                                                console.log(item);
+                                                var pid = item.id;
+                                                videos = '<li class="erase"><a class="tweetbutton"href="\"https://twitter.com/intent/tweet\"">Tweet</a><iframe src=\"//www.youtube.com/embed/'+pid+'\"</li>';
+                             //Append to results listStyleType
+                             $('#results').append(videos);
+
+                             })
+
+                      }
+                  );
+	 });
