f805e1e4e00dd4ed722c76017427f57ae76ed70d
Kartik Iyer <iyerkartik04@gmail.com>
2016-03-29 14:12:15 -0700

    Added files via upload
    diffrentiating between channels and videos.Channel object gave displaying errors.Added a check for videotype check.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(document).ready(function (){
 			})
 		}
 	);
-	$('#searchForm').on('submit', function(e){
+	 $('#searchForm').on('submit', function(e){
 	 	$(".erase").remove();
 	 	$.get(
 			"https://www.googleapis.com/youtube/v3/search",{
@@ -23,9 +23,14 @@ $(document).ready(function (){
 			key: 'AIzaSyAa1rfPGIPa7_LYPXkvPeE_5AIxwUgBHhE'},
 			function(data){
 					$.each(data.items,function(i,item){
-					var pid = item.id;
+					var kind = item.id.kind;
+					console.log(item);
+					console.log(pid);
+					if(kind ==='youtube#video'){
+					var pid = item.id.videoId;
 					output = '<li class="erase"><a class="tweetbutton"href="\"https://twitter.com/intent/tweet\"">Tweet</a><iframe src=\"//www.youtube.com/embed/'+pid+'\"</li>';
 					$('#results').append(output);
+					}
 					})
 			}
 		);
